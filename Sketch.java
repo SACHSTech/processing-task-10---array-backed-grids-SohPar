0ba99872ebc11c21d3c9fe90013cd02eb4fa47e0
SohPar <sohail.parpia23@ycdsbk12.ca>
2022-05-23 15:19:40 -0400

    Processing Task 10

--- Sketch.java
+++ Sketch.java
@@ -1,21 +1,31 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+
+  // Global Variables
+  int CELL_WIDTH = 20;
+  int CELL_HEIGHT = 20;
+  int MARGIN = 5;
+  int ROW_COUNT = 10;
+  int COLUMN_COUNT = 10;
+  int SCREEN_WIDTH;
+  int SCREEN_HEIGHT;
+  
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
     // put your size call here
-    size(500, 600);
+    size(300, 300);
   }
 
   /** 
    * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
+   * values here i.e background, stroke, fill etc. 
    */
   public void setup() {
-    background(255,255,255);
+    background(0);
   }
 
   /**
